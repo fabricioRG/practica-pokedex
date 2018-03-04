cfcef223508778a81da82a49ee130d37257808f0
fabricioRG <37033412+fabricioRG@users.noreply.github.com>
2018-03-03 23:18:11 -0600

    Add files via upload

---
+++ PokemonBeta/src/Pokemones/Pokemon.java
@@ -0,0 +1,46 @@
+
+package Pokemones;
+
+import java.util.Random;
+
+public class Pokemon {
+    
+    
+    String nombre;
+    int HP;
+    int nivel;
+    String habilidad;
+    String especie;
+    String tipo;
+    double peso;
+    double altura;
+    String habitat;
+    int aleatorio;
+
+    public Pokemon(String nombre, int HP, int nivel, String habilidad, String especie, String tipo, double peso, double altura, String habitat) {
+        this.nombre = nombre;
+        this.HP = HP;
+        this.nivel = nivel;
+        this.habilidad = habilidad;
+        this.especie = especie;
+        this.tipo = tipo;
+        this.peso = peso;
+        this.altura = altura;
+        this.habitat = habitat;
+    }
+    
+    //Metodo que selecciona aleatoriamente a un pokemon
+    public void EleccionPokemonInicio() {
+        Random pokemonRandom = new Random();
+        aleatorio = 1 + pokemonRandom.nextInt(5);
+        this.aleatorio =  aleatorio;
+    }
+    
+    public void InicioPokemon(){
+        EleccionPokemonInicio();
+        if (aleatorio == 1) {
+            
+        }
+    }
+    
+}
---
+++ PokemonBeta/src/Pokemones/PokemonAgua.java
@@ -0,0 +1,17 @@
+
+package Pokemones;
+/**
+ * Pokedex
+ * @author fabricio - 28.02.2018 
+ * @Title: PokemonAgua
+ * @Description: description
+ *
+ * Changes History
+ */
+
+public class PokemonAgua extends Pokemon {
+
+    public PokemonAgua(String nombre, int HP, int nivel, String habilidad, String especie, String tipo, double peso, double altura, String habitat) {
+        super(nombre, HP, nivel, habilidad, especie, tipo, peso, altura, habitat);
+    }    
+}
